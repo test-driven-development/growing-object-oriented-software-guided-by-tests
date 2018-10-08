01d04527979462926ca2979c93306155536a3f28
WIL PANNELL <wgpannel@us.ibm.com>
2018-10-08 17:25:56 -0500

    first commit

---
+++ src/test/java/com/auction/AuctionSniperEndToEndTest.java
@@ -0,0 +1,43 @@
+package com.auction;
+
+/*
+*
+import org.jmock.Expectations;
+import org.jmock.auto.Mock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+* */
+
+import com.auction.fakes.FakeAuctionServer;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AuctionSniperEndToEndTest {
+  private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+  private final ApplicationRunner application = new ApplicationRunner();
+
+  @Test
+  public void canary() {
+    assertThat(true).isEqualTo(true);
+  }
+
+  @Test
+  public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+    auction.startSellingItem();
+    application.startBiddingIn(auction);
+    auction.hasReceivedJoinRequestFromSniper();
+    auction.announceClosed();
+    application.showsSniperHasLostAuction();
+  }
+
+  //@After
+  public void stopAuction() {
+    auction.stop();
+  }
+
+  //@After
+  public void stopApplication() {
+    auction.stop();
+  }
+}
